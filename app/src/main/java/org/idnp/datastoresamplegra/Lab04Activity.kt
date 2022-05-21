2a6be48f3a5efe252abaf574b1f3b949d362afd2
diegoflores20173372 <dflorescam@unsa.edu.pe>
2022-05-20 22:12:38 -0500

    Final Stage

--- app/src/main/java/org/idnp/datastoresamplegra/Lab04Activity.kt
+++ app/src/main/java/org/idnp/datastoresamplegra/Lab04Activity.kt
@@ -2,14 +2,17 @@ package org.idnp.datastoresamplegra
 
 import android.content.Context
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
+import android.graphics.fonts.Font
 import android.os.Bundle
-import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+
 
 class Lab04Activity : AppCompatActivity() {
     private val Context.dataStore by preferencesDataStore(Lab04Data.DATA_NAME)
@@ -20,12 +23,62 @@ class Lab04Activity : AppCompatActivity() {
 
         dataLab04 = Lab04Data(dataStore)
 
+        val edtPA: EditText = findViewById(R.id.edtPA)
+        val edtEP: EditText = findViewById(R.id.edtEP)
+        val edtCA: EditText = findViewById(R.id.edtCA)
+        val edtNA: EditText = findViewById(R.id.edtNA)
+        val edtS: EditText = findViewById(R.id.edtS)
+        val edtD: EditText = findViewById(R.id.edtD)
+        val btnSaveInfo: Button = findViewById(R.id.btnSaveFormData)
+        lifecycleScope.launch {
+            dataLab04.academicPeriod.collect { aP ->
+                edtPA.setText(aP)
+            }
+        }
+        lifecycleScope.launch {
+            dataLab04.professionalSchool.collect { pS ->
+                edtEP.setText(pS)
+            }
+        }
+        lifecycleScope.launch {
+            dataLab04.courseCode.collect { cC ->
+                edtCA.setText(cC)
+            }
+        }
+        lifecycleScope.launch {
+            dataLab04.courseName.collect { cN ->
+                edtNA.setText(cN)
+            }
+        }
+        lifecycleScope.launch {
+            dataLab04.semester.collect { s ->
+                edtS.setText(s)
+            }
+        }
+        lifecycleScope.launch {
+            dataLab04.duration.collect { d ->
+                edtD.setText(d)
+            }
+        }
+        btnSaveInfo.setOnClickListener {
+            lifecycleScope.launch {
+                dataLab04.saveDataCourse(
+                    edtPA.text.toString(),
+                    edtEP.text.toString(),
+                    edtCA.text.toString(),
+                    edtNA.text.toString(),
+                    edtS.text.toString(),
+                    edtD.text.toString()
+                )
+            }
+        }
+
         //Change background color
-        var layoutFull: LinearLayout = findViewById(R.id.mainContainer)
+        val layoutFull: LinearLayout = findViewById(R.id.mainContainer)
         val edtColorName: EditText = findViewById(R.id.edtColorBack)
         val buttonColorBack: Button = findViewById(R.id.btnColorBack)
         buttonColorBack.setOnClickListener {
-            when(edtColorName.text.toString()) {
+            when (edtColorName.text.toString()) {
                 "negro" -> {
                     lifecycleScope.launch {
                         dataLab04.saveNoteBackgroundColor(Color.BLACK.toString())
@@ -74,11 +127,144 @@ class Lab04Activity : AppCompatActivity() {
                         }
                     }
                 }
+                else -> {
+                    lifecycleScope.launch {
+                        dataLab04.backgroundColor.collect { myColor ->
+                            layoutFull.setBackgroundColor(Integer.parseInt(myColor.toString()))
+                        }
+                    }
+                }
             }
         }
 
         //Change Font
+        val txtPA: TextView = findViewById(R.id.txtPA)
+        val txtEP: TextView = findViewById(R.id.txtEP)
+        val txtCA: TextView = findViewById(R.id.txtCA)
+        val txtNA: TextView = findViewById(R.id.txtNA)
+        val txtS: TextView = findViewById(R.id.txtS)
+        val txtD: TextView = findViewById(R.id.txtD)
+        val txtChangeBack: TextView = findViewById(R.id.txtChangeBackColor)
+        val txtChangeFont: TextView = findViewById(R.id.txtChangeFont)
+        val txtChangeSize: TextView = findViewById(R.id.txtChangeSize)
+        val edtFont: EditText = findViewById(R.id.edtFont)
+        val buttonFont: Button = findViewById(R.id.btnFont)
+        buttonFont.setOnClickListener {
+            when (edtFont.text.toString()) {
+                "Black" -> {
+                    lifecycleScope.launch {
+                        dataLab04.saveFontText("sans-serif-black")
+                        dataLab04.fontText.collect { myFont ->
+                            txtPA.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtEP.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtCA.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtNA.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtS.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtD.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtChangeBack.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtChangeFont.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtChangeSize.typeface = Typeface.create(myFont, Typeface.BOLD)
+                        }
+                    }
+                }
+                "Monospace" -> {
+                    lifecycleScope.launch {
+                        dataLab04.saveFontText("sans-serif-condensed")
+                        dataLab04.fontText.collect { myFont ->
+                            txtPA.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtEP.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtCA.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtNA.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtS.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtD.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtChangeBack.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtChangeFont.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            txtChangeSize.typeface = Typeface.create(myFont, Typeface.BOLD)
+                        }
+                    }
+                }
+                "Sans Serif" -> {
+                    lifecycleScope.launch {
+                        dataLab04.saveFontText("sans-serif-medium")
+                        dataLab04.fontText.collect { myFont ->
+                            txtPA.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                            txtEP.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                            txtCA.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                            txtNA.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                            txtS.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                            txtD.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                            txtChangeBack.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                            txtChangeFont.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                            txtChangeSize.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                        }
+                    }
+                }
+                else -> {
+                    lifecycleScope.launch {
+                        dataLab04.fontText.collect { myFont ->
+                            if (myFont == "sans-serif-medium") {
+                                txtPA.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                                txtEP.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                                txtCA.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                                txtNA.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                                txtS.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                                txtD.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                                txtChangeBack.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                                txtChangeFont.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                                txtChangeSize.typeface = Typeface.create(myFont, Typeface.NORMAL)
+                            } else {
+                                txtPA.typeface = Typeface.create(myFont, Typeface.BOLD)
+                                txtEP.typeface = Typeface.create(myFont, Typeface.BOLD)
+                                txtCA.typeface = Typeface.create(myFont, Typeface.BOLD)
+                                txtNA.typeface = Typeface.create(myFont, Typeface.BOLD)
+                                txtS.typeface = Typeface.create(myFont, Typeface.BOLD)
+                                txtD.typeface = Typeface.create(myFont, Typeface.BOLD)
+                                txtChangeBack.typeface = Typeface.create(myFont, Typeface.BOLD)
+                                txtChangeFont.typeface = Typeface.create(myFont, Typeface.BOLD)
+                                txtChangeSize.typeface = Typeface.create(myFont, Typeface.BOLD)
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-
+        //Change Size
+        //We use the same TextView in the last exercise
+        val edtSize: EditText = findViewById(R.id.edtSize)
+        val buttonSize: Button = findViewById(R.id.btnSize)
+        buttonSize.setOnClickListener {
+            val fontSize = edtSize.text.toString()
+            if (fontSize == "") {
+                lifecycleScope.launch {
+                    dataLab04.sizeText.collect { mySizeText ->
+                        txtPA.textSize = mySizeText
+                        txtEP.textSize = mySizeText
+                        txtCA.textSize = mySizeText
+                        txtNA.textSize = mySizeText
+                        txtS.textSize = mySizeText
+                        txtD.textSize = mySizeText
+                        txtChangeBack.textSize = mySizeText
+                        txtChangeFont.textSize = mySizeText
+                        txtChangeSize.textSize = mySizeText
+                    }
+                }
+            } else {
+                lifecycleScope.launch {
+                    dataLab04.saveSizeText(fontSize.toFloat())
+                    dataLab04.sizeText.collect { mySizeText ->
+                        txtPA.textSize = mySizeText
+                        txtEP.textSize = mySizeText
+                        txtCA.textSize = mySizeText
+                        txtNA.textSize = mySizeText
+                        txtS.textSize = mySizeText
+                        txtD.textSize = mySizeText
+                        txtChangeBack.textSize = mySizeText
+                        txtChangeFont.textSize = mySizeText
+                        txtChangeSize.textSize = mySizeText
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/org/idnp/datastoresamplegra/Lab04Data.kt
+++ app/src/main/java/org/idnp/datastoresamplegra/Lab04Data.kt
@@ -1,28 +1,102 @@
 package org.idnp.datastoresamplegra
 
 import android.graphics.Color
+import android.graphics.Typeface
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import java.util.*
 
 class Lab04Data(private val dataStore: DataStore<Preferences>) {
+
+    suspend fun saveDataCourse(
+        pa: String,
+        ep: String,
+        ca: String,
+        na: String,
+        s: String,
+        d: String
+    ) {
+        dataStore.edit { preferences ->
+            preferences[ACADEMIC_PERIOD] = pa
+            preferences[PROFESSIONAL_SCHOOL] = ep
+            preferences[COURSE_CODE] = ca
+            preferences[COURSE_NAME] = na
+            preferences[SEMESTER] = s
+            preferences[DURATION] = d
+        }
+    }
+
     suspend fun saveNoteBackgroundColor(noteBackgroundColor: String) {
         dataStore.edit { preferences ->
             preferences[BACKGROUND_COLOR] = noteBackgroundColor
         }
     }
 
+    suspend fun saveFontText(fontText: String) {
+        dataStore.edit { preferences ->
+            preferences[FONT_TEXT] = fontText
+        }
+    }
+
+    suspend fun saveSizeText(sizeText: Float) {
+        dataStore.edit { preferences ->
+            preferences[SIZE_TEXT] = sizeText
+        }
+    }
+
+    val academicPeriod: Flow<String>
+        get() = dataStore.data.map { preferences ->
+            preferences[ACADEMIC_PERIOD] ?: ""
+        }
+    val professionalSchool: Flow<String>
+        get() = dataStore.data.map { preferences ->
+            preferences[PROFESSIONAL_SCHOOL] ?: ""
+        }
+    val courseCode: Flow<String>
+        get() = dataStore.data.map { preferences ->
+            preferences[COURSE_CODE] ?: ""
+        }
+    val courseName: Flow<String>
+        get() = dataStore.data.map { preferences ->
+            preferences[COURSE_NAME] ?: ""
+        }
+    val semester: Flow<String>
+        get() = dataStore.data.map { preferences ->
+            preferences[SEMESTER] ?: ""
+        }
+    val duration: Flow<String>
+        get() = dataStore.data.map { preferences ->
+            preferences[DURATION] ?: ""
+        }
     val backgroundColor: Flow<String>
         get() = dataStore.data.map { preferences ->
             preferences[BACKGROUND_COLOR] ?: Color.CYAN.toString()
         }
+    val fontText: Flow<String>
+        get() = dataStore.data.map { preferences ->
+            preferences[FONT_TEXT] ?: Typeface.NORMAL.toString()
+        }
+    val sizeText: Flow<Float>
+        get() = dataStore.data.map { preferences ->
+            preferences[SIZE_TEXT] ?: 14.0f
+        }
 
     companion object {
         const val DATA_NAME = "DATA_NAME"
+
+        //Variable to Data Info
+        private val ACADEMIC_PERIOD = stringPreferencesKey("academic_period")
+        private val PROFESSIONAL_SCHOOL = stringPreferencesKey("professional_school")
+        private val COURSE_CODE = stringPreferencesKey("course_code")
+        private val COURSE_NAME = stringPreferencesKey("course_name")
+        private val SEMESTER = stringPreferencesKey("semester")
+        private val DURATION = stringPreferencesKey("duration")
+
+        //Variable to edit style
         private val BACKGROUND_COLOR = stringPreferencesKey("key_app_background_color")
-        private val FONT_TEXT = stringPreferencesKey("key_app_font")
+        private val FONT_TEXT = stringPreferencesKey("key_app_font_text")
+        private val SIZE_TEXT = floatPreferencesKey("key_app_size_text")
     }
 }
